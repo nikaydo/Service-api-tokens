fc164dbc9b62fbeacc9f5d76e05866c50b52b5d7
Nikaydo <sooosooor49@gmail.com>
2025-06-10 02:06:33 +0500

    update

--- cmd/main.go
+++ cmd/main.go
@@ -18,30 +18,30 @@ import (
 func main() {
 	env, err := config.ReadEnv()
 	if err != nil {
-		log.Fatal("Error loading apiTokens .env file:", err)
+		log.Fatal("Error loading .env file:", err)
 	}
-	log.Println("Database apiTokens succesful read")
+	log.Println("Database succesful read")
 	db, err := database.DatabaseInit(env)
 	if err != nil {
-		log.Fatal("Error loading apiTokens .env file:", err)
+		log.Fatal("Error loading .env file:", err)
 	}
-	log.Println("Database apiTokens succesful connected")
+	log.Println("Database succesful connected")
 	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%s", env.EnvMap["HOST"], env.EnvMap["PORT"]))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	grpcServer := grpc.NewServer()
 	apiTokens.RegisterApiTokenServer(grpcServer, &au.ApiTokenService{Db: db})
-	log.Println("gRPC apiTokens server started on ", fmt.Sprintf("%s:%s", env.EnvMap["HOST"], env.EnvMap["PORT"]))
+	log.Println("gRPC server started on ", fmt.Sprintf("%s:%s", env.EnvMap["HOST"], env.EnvMap["PORT"]))
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	go func() {
 		if err := grpcServer.Serve(lis); err != nil {
-			log.Fatalf("failed to serve apiTokens : %v", err)
+			log.Fatalf("failed to serve: %v", err)
 		}
 	}()
 	<-quit
-	log.Println("Shutting down apiTokens server...")
+	log.Println("Shutting down server...")
 	grpcServer.GracefulStop()
-	log.Println("Server apiTokens gracefully stopped")
+	log.Println("Server gracefully stopped")
 }
